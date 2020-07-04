cb22ed20439f082da403961418ffc81086354c2b
shehan99099 <62324980+shehan99099@users.noreply.github.com>
2020-07-04 11:38:38 +0530

    Update aaa.sh

--- aaa.sh
+++ aaa.sh
@@ -5,15 +5,38 @@ $(openssl aes-256-cbc -a -salt -in /dev/stdin -out /dev/stdout <<< "${script}")
 EOF2
 
 read -s -p "Enter script password: " password
-echo
-unencrypted_script=\$(openssl aes-256-cbc -d -a -salt -in /dev/stdin -out /dev/stdout <<< "\${encrypted_script}" -pass pass:"\${password}" 2>/dev/null | tr -d '\000')
 clear
-checksum="\$(echo "\$unencrypted_script" | md5sum | awk '{ print \$1 }')"
-if [ "\${checksum}" = "${checksum}" ]; then
-    eval "\${unencrypted_script}"
-    exit 0
-else
-    echo "Wrong password inserted"
-    exit 1
-fi
-EOF
+#dont copyright or mod this script
+#shehan lahiru
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+pin='\033[36;1m' #pink
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+echo
+echo
+figlet -f slant "W E B
+        D O C T O R"|lolcat
+sleep 5
+echo
+echo $i"["$bi"¤"$i"]"$me"───────────────────────────────────────────"$i"["$bi"¤"$i"]"
+echo $i" |"$cy"  WEB DOCTOR V1.1 UPDATE COMING UP 10/07/2020"$i"|"
+echo $i" |"$me"─────────────────────────────────────────────"$i"|"
+echo $i" |"$pu" FACEBOOK :"$ku" shehan lahiru"$i"                    |"
+echo $i" |"$pu" Contack WhatsApp :"$ku" +94774593440"$i"             |"
+echo $i"["$bi"¤"$i"]"$me"───────────────────────────────────────────"$i"["$bi"¤"$i"]"
+echo
+echo $pu"    ─────|─────────────────────────────|─────"
+echo $i"         |"$me" 1"$i" |"$pu" METASPLOIT v6 INSTALL.  "$i"|"
+echo $i"         |"$cy" 2"$i" |"$pin" METASPLOIT v9 INSTALL.  "$i"|"
+echo $i"         |"$pu" 3"$i" |"$pin" INSTALL KALI LINUX.     "$i"|"
+echo $i"         |"$purl" 4"$i" |"$pin" INSTALL UBUNTU.         "$i"|"
+echo $i"         |"$ku" 5"$i" |"$bi" UNINSTALL UBUNTU.       "$i"|"
+echo $i"         |"$i" 6"$i" |"$i" UNINSTALL KALI.         "$i"|"
+echo $i"         |"$bi" 7"$i" |"$pur" INSTALL PKGS.           "$i"|"
+echo $i"         |"$cy" 8"$i" |"$pin" UPDATE.                 "$i"|"
+echo $i"         |"$i" 9"$i" |"$i" Location coming up      "$i"|"
+echo $i"         |─────────────────────────────|"
+echo $i"         |"
